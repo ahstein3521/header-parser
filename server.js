d0da6f1c4d2b52810efe32b4b8c3e8172f225d63
ahs3521 <adamhs3521@gmail.com>
2016-07-02 12:44:21 -0400

    fixed parsing for software

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-var os=require('os');
 var app=require('express')()
-
 var requestIp = require('request-ip');
-
-
 
 
 var port= process.env.PORT||3000;
@@ -11,13 +7,10 @@ var port= process.env.PORT||3000;
 app.listen(port);
 app.use(requestIp.mw())
 
-app.route('/').get(function(req,res){
-   var ip = req.clientIp, 
-     lang=req.headers["accept-language"].split(',')[0],
-     system=os.type().replace('_'," ")+' '+os.release()+'; '+os.platform()+'; '+os.arch()+";"
+app.get('/',function(req,res){
+     language=req.headers["accept-language"].split(',')[0],
+     software=req.headers["user-agent"].split("(")[1].split(")")[0];
    
-   
-   
-   res.json({ip:ip,language:lang,software:system})
+   res.json({ip:req.clientIp, language, software})
 
 })
